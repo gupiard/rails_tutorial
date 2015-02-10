4dff974b9c85dcb93263016468b12f3431ad55ae
gupiard <guillaume.piard@gmail.com>
2015-02-09 18:22:11 -0600

    Fix public key error

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJKSMWAHY6QTATYNQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['railstutorialgui']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
